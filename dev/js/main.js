70ed52883b7d31b2a08ef13d383ce5895121daba
ayush bhargava <57057106+ayushbhargava22@users.noreply.github.com>
2020-04-28 21:43:01 +0530

    Add files via upload

--- dev/js/main.js
+++ dev/js/main.js
@@ -2,4 +2,16 @@ function enquiry() {
     alert("your query has been submitted , we will notify you on your email")
 }
 
-$("input[name='expiry-data']").mask("00 / 00");
+
+
+document.getElementById("mobile-menu-button").addEventListener('click', function() {
+    var box3 = document.getElementById("js-mobile-menu")
+    if(box3.style.display=="none")
+    {
+        box3.style.display = "block"
+    }
+    else
+    {
+        box3.style.display = "none"
+    }
+})
